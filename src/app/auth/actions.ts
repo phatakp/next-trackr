e2c26ba8b8a936b75440a0666bb35d0f3bb1dbb7
Praveen Phatak <praveenphatak@gmail.com>
2024-07-23 09:14:05 +0530

    vercel issues

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -1,19 +1,8 @@
 "use server";
 import { createClient } from "@/lib/supabase/server";
+import { getURL } from "@/lib/utils";
 import { redirect } from "next/navigation";
 import { LoginSchema, RegisterSchema } from "./zod-schemas";
-
-const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000/";
-  // Make sure to include `https://` when not localhost.
-  url = url.startsWith("http") ? url : `https://${url}`;
-  // Make sure to include a trailing `/`.
-  url = url.endsWith("/") ? url : `${url}/`;
-  return url;
-};
 
 export async function loginWithPassword(input: Zod.infer<typeof LoginSchema>) {
   const supabase = await createClient();
@@ -31,7 +20,7 @@ export async function register(input: Zod.infer<typeof RegisterSchema>) {
   const { error } = await supabase.auth.signUp({
     ...input,
     options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/confirm`,
+      emailRedirectTo: `${getURL()}/auth/confirm`,
     },
   });
   if (error) return { error: error.message, data: null };
@@ -43,7 +32,7 @@ export async function loginWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+      redirectTo: `${getURL()}/auth/callback`,
     },
   });
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,21 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+
+  // Make sure to include `https://` when not localhost.
+  if (process.env.NODE_ENV === "production") {
+    url = url.startsWith("http") ? url : `https://${url}`;
+  }
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
+
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
