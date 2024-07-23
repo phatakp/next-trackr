f3f3678e54f82de14f205aa9ea73fe33a04068d6
Praveen Phatak <praveenphatak@gmail.com>
2024-07-23 09:26:47 +0530

    log site url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,14 +10,16 @@ export const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000/";
+    process.env.NODE_ENV === "production"
+      ? "https://trackr-finance.vercel.app"
+      : "http://localhost:3000";
 
   // Make sure to include `https://` when not localhost.
   if (process.env.NODE_ENV === "production") {
-    url = url.startsWith("http") ? url : `https://${url}`;
+    url = url.startsWith("http:") ? url : `https://${url}`;
   }
-  // Make sure to include a trailing `/`.
-  url = url.endsWith("/") ? url : `${url}/`;
+  console.log("url:", url);
+
   return url;
 };
 
